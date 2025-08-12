91b1a23a31957c2c1a11bd5335a2a5a511c0c080
Ilya <ivan.ivan16@inbox.ru>
2025-08-12 12:43:51 +0300

    new Components

---
+++ src/companents/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <div className="header">
+            <header className="header">
+                <img className='img' src='https://png.pngtree.com/png-clipart/20190611/original/pngtree-wolf-logo-png-image_2306634.jpg'/>
+            </header>
+        </div>
+    )
+}
+export default Header;
